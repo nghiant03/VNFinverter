feb891fb7e36b10cb0dc49c0fe1fd1556a3d4a11
Sinner <nghiant@mailbox.org>
2025-08-11 01:40:50 +0700

    feat: add cli

---
+++ vnfinverter/cli.py
@@ -0,0 +1,19 @@
+import argparse
+from pathlib import Path
+from vnfinverter.parser import parse_pdf
+from vnfinverter.converter import to_ofx
+
+def main():
+    parser = argparse.ArgumentParser(description="VNFinverter")
+    parser.add_argument("--path", help="Path to PDF", required=True)
+    parser.add_argument("--out", help="Output file path", required=True)
+    args = parser.parse_args()
+
+    statement = parse_pdf(Path(args.path))
+    ofx_content = to_ofx(statement)
+
+    with open(args.out, "w") as f:
+        f.write(ofx_content)
+
+if __name__ == "__main__":
+    main()
